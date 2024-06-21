8f59b3ac811f8033c64e59d0d131bfaa579d17f8
Marthen <dodimartin.sit@gmail.com>
2024-06-21 16:44:01 +0700

    learn variable

--- main.c
+++ main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 
+
+int x = 20;
+void functionSum (){
+    printf("Sum example : %d\n", x + 10); 
+    }
+void functionMul (){
+    printf("Multiple from example : %d\n", x * 10);
+    }
+
 int main()
 {
-    int age = 25;
-    printf("Age: %d", age);
+    /*int defined_var;
+    printf("Defined_ar: %d\n", defined_var);
+
+    defined_var = 10;
+    int new_var = 14;
+    printf("Value of defined var: %d\n", defined_var);
+    printf("value of new var: %d", new_var);
+    return 0;*/
+    functionSum();
+    functionMul();
     return 0;
+
 }
